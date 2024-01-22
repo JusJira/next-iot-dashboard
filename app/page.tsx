7947a8bc961ac801f3da9db7fcc210873bf13b46
JustinJira <jus.jira@gmail.com>
2024-01-22 15:01:05 +0700

    style: Add Skeleton UI

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,15 @@ import dynamic from "next/dynamic";
 import { Gauge } from "@/components/gauge";
 import { DateTime } from "luxon";
 import { ModeToggle } from "@/components/themeToggle";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const fetcher = (url: string | URL | Request) =>
   fetch(url).then((res) => res.json());
 export default function Home() {
-  const { data, isLoading } = useSWR("/api/getLast", fetcher, {
+  const { data, isLoading, error } = useSWR("/api/getLast", fetcher, {
     refreshInterval: 10000,
   });
-  console.log(data);
-  if (isLoading) return null;
+  if (error) return <div>failed to load</div>;
   return (
     <>
       <header className="h-[80px] flex items-center justify-end px-4">
@@ -30,9 +30,13 @@ export default function Home() {
               <h2 className="font-semibold text-2xl">Temperature</h2>
             </div>
             <div className="flex items-center justify-center m-auto pb-6">
-              <span className="text-3xl animate-gauge_fadeIn">
-                {data.temperature.toFixed(1)} &deg;C
-              </span>
+              {isLoading ? (
+                <Skeleton className="w-[128px] h-[2.25rem]"/>
+              ) : (
+                <span className="text-3xl animate-gauge_fadeIn">
+                  {data.temperature.toFixed(1)} &deg;C
+                </span>
+              )}
             </div>
           </div>
           <div className="border rounded-lg shadow-sm relative flex flex-col  aspect-square ">
@@ -40,21 +44,29 @@ export default function Home() {
               <h2 className="font-semibold text-2xl">Humidity</h2>
             </div>
             <div className="flex items-center justify-center m-auto pb-6">
-              <Gauge
-                value={data.humidity.toFixed()}
-                showValue
-                size="large"
-                sign="%"
-              />
+              {isLoading ? (
+                <Skeleton className="w-[108px] h-[108px] rounded-full"/>
+              ) : (
+                <Gauge
+                  value={data.humidity.toFixed()}
+                  showValue
+                  size="large"
+                  sign="%"
+                />
+              )}
             </div>
           </div>
         </div>
-        <span className="text-sm text-center sm:text-base">
-          Last Updated :{" "}
-          {DateTime.fromISO(data.time).toLocaleString(
-            DateTime.DATETIME_FULL_WITH_SECONDS
-          )}
-        </span>
+        {isLoading ? (
+          <Skeleton className="w-[32rem] h-4 sm:h-6"/>
+        ) : (
+          <span className="text-sm text-center sm:text-base">
+            Last Updated :{" "}
+            {DateTime.fromISO(data.time).toLocaleString(
+              DateTime.DATETIME_FULL_WITH_SECONDS
+            )}
+          </span>
+        )}
       </main>
       <footer className="h-[80px]"></footer>
     </>
